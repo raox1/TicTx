92ce36c56189c3b062c267ced2852cdd9946541c
Lalit Rao <lalitkumaryadav9090@gmail.com>
2025-01-17 12:17:06 +0530

    Initial Commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Lalit Kumar on 17/01/25.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
