c854797cebb3eca0504b9a5fee7f6cdca2541790
Lalit Rao <lalitkumaryadav9090@gmail.com>
2025-01-17 14:02:30 +0530

    Updated Game

--- TicTacToe/Alerts.swift
+++ TicTacToe/Alerts.swift
@@ -5,4 +5,26 @@
 //  Created by Lalit Kumar on 17/01/25.
 //
 
-import Foundation
+import SwiftUI
+
+struct AlertItem: Identifiable {
+    let id = UUID()
+    var title: Text
+    var message: Text
+    var buttonTitle: Text
+}
+
+struct AlertContext {
+    static let humanWin = AlertItem(title: Text("You Win!"),
+                             message: Text("You are smart."),
+                             buttonTitle: Text("hell Yeah!"))
+    
+    static let computerWin = AlertItem(title: Text("You Lost!"),
+                             message: Text("Ai more smart."),
+                             buttonTitle: Text("hell nah!"))
+    
+    static let draw = AlertItem(title: Text("Draw!"),
+                             message: Text("Both Smart"),
+                             buttonTitle: Text("Try Again!"))
+}
+
--- TicTacToe/ContentView.swift
+++ TicTacToe/ContentView.swift
@@ -7,15 +7,134 @@
 
 import SwiftUI
 
+
+
+
+
 struct ContentView: View {
+    let columns : [GridItem] = [GridItem(.flexible()),
+                                GridItem(.flexible()),
+                                GridItem(.flexible()),]
+
+    @State private var moves : [Move?] = Array(repeating: nil, count: 9)
+    @State private var isGameboardDisabled = false
+    @State private var alertItem: AlertItem?
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        GeometryReader { geometry in
+            
+            VStack {
+                Spacer()
+                LazyVGrid(columns: columns, spacing: 5){
+                    
+                    ForEach(0..<9) { i in
+                        ZStack {
+                            Circle()
+                                .foregroundStyle(.pink)
+                                .opacity(0.7)
+                                .frame(width: geometry.size.width/3 - 15, height: geometry.size.width/3 - 15)
+                            Image(systemName: moves[i]?.indicator ?? "")
+                                .resizable()
+                                .frame(width: 40, height: 40)
+                                .foregroundStyle(.white)
+                        }.onTapGesture {
+                            
+                            if isSquareOccupied(in: moves, forIndex: i) {
+                                return
+                            }
+                            moves[i] = Move(player: .human, boardIndex: i)
+                            
+                            
+                            // check for win condition or draw
+                            
+                            if checkWinCondition(for: .human, in: moves ){
+                                alertItem = AlertContext.humanWin
+                                return
+                            }
+                            
+                            if checkForDraw(in: moves){
+                                alertItem = AlertContext.draw
+                                return
+                            }
+                            isGameboardDisabled = true
+                            
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                                let computerPosition = determineComputerMovePosition(in: moves)
+                                
+                                moves[computerPosition] = Move(player: .computer, boardIndex: computerPosition )
+                                
+                                isGameboardDisabled = false
+                                
+                                if checkWinCondition(for: .computer, in: moves ){
+                                    alertItem = AlertContext.computerWin
+                                    return
+                                }
+                                if checkForDraw(in: moves){
+                                    alertItem = AlertContext.draw
+                                    return
+                                }
+                            }
+                        }
+                        
+                    }
+                }
+                Spacer()
+            }
+            .disabled(isGameboardDisabled)
+            .padding()
+            .alert(item: $alertItem, content: {alertItem in
+                Alert(title: alertItem.title, message: alertItem.message, dismissButton: .default(alertItem.buttonTitle, action: {
+                    resetGame()
+                }))
+            }
+            )
         }
-        .padding()
+    }
+    func isSquareOccupied(in move: [Move?], forIndex index: Int) -> Bool {
+        return moves.contains(where: { $0?.boardIndex == index })
+    }
+    
+    func determineComputerMovePosition(in moves: [Move?]) -> Int {
+        var movePosition = Int.random(in: 0..<9)
+        
+        while isSquareOccupied(in: moves, forIndex: movePosition) {
+            movePosition = Int.random(in: 0..<9)
+        }
+        
+        return movePosition
+    }
+    
+    func checkWinCondition(for player: Player, in moves: [Move?]) -> Bool {
+        let winPatterns: Set<Set<Int>> = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
+        
+        let playerMoves = moves.compactMap{ $0 }.filter { $0.player == player}
+        let playerPositions = Set(playerMoves.map { $0.boardIndex })
+        
+        for pattern in winPatterns where pattern.isSubset(of: playerPositions) {
+            return true
+        }
+        return false
+    }
+    
+    func checkForDraw(in moves: [Move?]) -> Bool {
+        return moves.compactMap{ $0 }.count == 9
+    }
+                   
+                   func resetGame() {
+                moves = Array(repeating: nil, count: 9)
+            }
+}
+
+enum Player {
+    case human, computer
+}
+
+struct Move {
+    let player: Player
+    let boardIndex: Int
+    
+    var indicator: String{
+        return player == .human ? "xmark" : "circle"
     }
 }
 
